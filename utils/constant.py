5199290ba3ccea878d8870dad0b22938bbd19c49
Huei Hwang <161418443+LimHueiHwang@users.noreply.github.com>
2026-08-13 22:59:17 +0800

    Add files via upload

---
+++ utils/constant.py
@@ -0,0 +1,3 @@
+MAX_RETRIES = 10
+target_wb_name = "PO Softcopy creation Imac Version 1.7 - for Excel 2016(SAP 770) - 07242025.xlsm"
+error_file = r"\\sgsind0nsifsv01a\IMAC\MACROS\test\Po Softcopy\python error PO softcopy.xlsx"
